19d9f19eeda3f42d2eff44a603b75c661b74b0fd
Macrina Vollmer <mvollmer001@student.franciscan.edu>
2024-11-19 11:41:21 -0500

    storytime

---
+++ code.py
@@ -0,0 +1,10 @@
+#f-strings + functions 
+#tkinter- big box 
+#text to speech 
+
+#Every year, we make __noun__ at Christmas time. __verb ending in ing__ has been a tradition since I was a/an __adj__ kid!
+#__Family member__ used to make most of the recipe back then, but I would always help__ verb__ __ ingredient__. 
+# Now that I'm older, I make the entire batch of __noun__ from scratch. 
+#All you have to do is mix __noun__ and __noun__ in a bowl until fluffy, and add __noun__. 
+# Don't forget the __noun__! __Verb__ them on a __noun__ and bake them at a __number__ degrees. 
+# After __ number__  minutes, you will have the perfect __noun__!
