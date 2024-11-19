e8d070b6be63b81c879a9a5658c9acba7050cd71
Macrina Vollmer <mvollmer001@student.franciscan.edu>
2024-11-19 11:52:45 -0500

    working

--- code.py
+++ code.py
@@ -1,6 +1,17 @@
 #f-strings + functions 
 #tkinter- big box 
 #text to speech 
+#wk1- n/a
+#wk2-variable names, strings, user imput
+#wk3- functions
+#wk4- n/a
+#wk5- LOGIC--if empty- error result
+#wk6- list, set, tuple, dictionary printed
+#wk7- loops + accumulator pattern 
+#wk8- n/a
+#wk9- objects and classes, scope 
+#wk10- tkinter 1
+#wk11- tkinter 2
 
 #Every year, we make __noun__ at Christmas time. __verb ending in ing__ has been a tradition since I was a/an __adj__ kid!
 #__Family member__ used to make most of the recipe back then, but I would always help__ verb__ __ ingredient__. 
@@ -8,3 +19,18 @@
 #All you have to do is mix __noun__ and __noun__ in a bowl until fluffy, and add __noun__. 
 # Don't forget the __noun__! __Verb__ them on a __noun__ and bake them at a __number__ degrees. 
 # After __ number__  minutes, you will have the perfect __noun__!
+
+import tkinter as tk
+window= tk.Tk()
+window.title("Mad Libs")
+label1= tk.Label(
+    window, 
+    text= "Enter words below:"
+    foreground= "white"
+    background= "purple"
+    width= 20, 
+    height= 10
+)
+
+
+window.mainloop()
